dedb8fa07803bbde39657634a229debe5339033d
Zhening Feng <fengzhening1116@outlook.com>
2021-08-23 23:43:07 -0700

    add high score record
    Now the high score played will be recorded

--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,11 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = -1;
+        # Read high_score from file
+        with open("high_score.txt") as file:
+            self.high_score = int(file.read())
+
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -16,7 +21,8 @@ class Scoreboard(Turtle):
         self.write_score()
 
     def write_score(self):
-        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGN, font=FONT)
 
     def increase_score(self):
         self.score += 1
@@ -26,3 +32,13 @@ class Scoreboard(Turtle):
     def game_over(self):
         self.goto(0,0)
         self.write(f"Game Over", align=ALIGN, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            # Rewrite high score
+            with open("high_score.txt", mode = "w") as file:
+                file.write(str(self.high_score))
+
+        self.score = 0
+        self.write_score()
